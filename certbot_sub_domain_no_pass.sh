92e95a03613bc552e4aa16c518ef71858a7d639e
Evann BACALA <e.bacala@gmail.com>
2020-02-10 08:12:06 +0100

    added https for a static website

---
+++ certbot_sub_domain_no_pass.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $# -ne 3 ]]; then
+  echo 'You need 3 arguments : your subdomain name (please include your domain), your email adress and the repertory that hosts the website';
+  exit;
+fi
+
+apt update
+apt install -y nginx certbot python-certbot-nginx
+
+fuser -k 80/tcp
+service nginx restart
+
+export NGINX_DOMAIN_NAME=$1
+export NGINX_EMAIL_ADDRESS=$2
+export NGINX_REPERTORY=$3
+
+# Launch Certbot with the domain name and email address defined in the environment variables
+certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agree-tos --no-eff-email
+
+# Replace the default template file with the environment variables
+envsubst '${NGINX_DOMAIN_NAME} ${NGINX_SUB_DOMAIN_NAME} ${NGINX_REPERTORY}' < certbot_sub_domain_no_pass.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
+ln -s /etc/nginx/sites-available/${NGINX_DOMAIN_NAME} /etc/nginx/sites-enabled/${NGINX_DOMAIN_NAME}
+
+# Reload the nginx configuration
+nginx -s reload
