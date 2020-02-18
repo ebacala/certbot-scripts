db0ba8cd0b8b7eed49acb5c9961ce5247fbf36a0
Evann BACALA <e.bacala@gmail.com>
2020-02-18 14:57:37 +0100

    added dh parameters

--- certbot_sub_domain_no_pass.sh
+++ certbot_sub_domain_no_pass.sh
@@ -18,6 +18,9 @@ export NGINX_REPERTORY=$3
 # Launch Certbot with the domain name and email address defined in the environment variables
 certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agree-tos --no-eff-email
 
+# Generate new DH parameters
+openssl dhparam -out /etc/letsencrypt/live/${NGINX_DOMAIN_NAME}/dhparam.pem 4096
+
 # Replace the default template file with the environment variables
 envsubst '${NGINX_DOMAIN_NAME} ${NGINX_SUB_DOMAIN_NAME} ${NGINX_REPERTORY}' < certbot_sub_domain_no_pass.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
 ln -s /etc/nginx/sites-available/${NGINX_DOMAIN_NAME} /etc/nginx/sites-enabled/${NGINX_DOMAIN_NAME}
