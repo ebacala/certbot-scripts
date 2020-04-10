c46d803c12103c9d534853f91a53d26ff6a9cace
Evann BACALA <evann.bacala@epsi.fr>
2020-04-10 07:58:16 +0200

    renamed for better view

--- certbot_sub_domain_static.sh
+++ certbot_sub_domain_static.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# -ne 3 ]]; then
-  echo 'You need 3 arguments : your subdomain name (please include your domain), your email adress and the port that needs to be forwarded';
+  echo 'You need 3 arguments : your subdomain name (please include your domain), your email adress and the repertory that hosts the website';
   exit;
 fi
 
@@ -13,7 +13,7 @@ service nginx restart
 
 export NGINX_DOMAIN_NAME=$1
 export NGINX_EMAIL_ADDRESS=$2
-export NGINX_APP_PORT=$3
+export NGINX_REPERTORY=$3
 
 # Launch Certbot with the domain name and email address defined in the environment variables
 certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agree-tos --no-eff-email
@@ -22,11 +22,10 @@ certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agr
 openssl dhparam -out /etc/letsencrypt/live/${NGINX_DOMAIN_NAME}/dhparam.pem 2048
 
 # Replace the default template file with the environment variables
-envsubst '${NGINX_DOMAIN_NAME} ${NGINX_SUB_DOMAIN_NAME} ${NGINX_APP_PORT}' < certbot_sub_domain.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
+envsubst '${NGINX_DOMAIN_NAME} ${NGINX_SUB_DOMAIN_NAME} ${NGINX_REPERTORY}' < certbot_sub_domain_static.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
 ln -s /etc/nginx/sites-available/${NGINX_DOMAIN_NAME} /etc/nginx/sites-enabled/${NGINX_DOMAIN_NAME}
 # Copy a new config file
 cp nginx.conf /etc/nginx/nginx.conf
 
 # Reload the nginx configuration
 nginx -s reload
-
