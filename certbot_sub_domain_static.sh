b179ba257b043909329d6597c234725e34c6fe07
Evann BACALA <evann.bacala@epsi.fr>
2020-05-30 16:22:05 +0200

    seek for user input, switched to absolute path for the repertory

--- certbot_sub_domain_static.sh
+++ certbot_sub_domain_static.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [[ $# -ne 3 ]]; then
-  echo 'You need 3 arguments : your subdomain name (please include your domain), your email adress and the repertory that hosts the website';
-  exit;
-fi
+echo "Please enter your domain name:"
+read NGINX_DOMAIN_NAME
+echo "Please enter your email adress (only used for certbot script):"
+read NGINX_EMAIL_ADDRESS
+echo "Please enter the repertory that will be used to store the static website (absolute path):"
+read NGINX_REPOSITORY
 
 apt update
 apt install -y nginx certbot python-certbot-nginx
@@ -11,9 +13,9 @@ apt install -y nginx certbot python-certbot-nginx
 fuser -k 80/tcp
 service nginx restart
 
-export NGINX_DOMAIN_NAME=$1
-export NGINX_EMAIL_ADDRESS=$2
-export NGINX_REPERTORY=$3
+export NGINX_DOMAIN_NAME=NGINX_DOMAIN_NAME
+export NGINX_EMAIL_ADDRESS=NGINX_EMAIL_ADDRESS
+export NGINX_REPOSITORY=NGINX_REPOSITORY
 
 # Launch Certbot with the domain name and email address defined in the environment variables
 certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agree-tos --no-eff-email
@@ -22,7 +24,7 @@ certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agr
 openssl dhparam -out /etc/letsencrypt/live/${NGINX_DOMAIN_NAME}/dhparam.pem 2048
 
 # Replace the default template file with the environment variables
-envsubst '${NGINX_DOMAIN_NAME} ${NGINX_SUB_DOMAIN_NAME} ${NGINX_REPERTORY}' < templates/certbot_sub_domain_static.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
+envsubst '${NGINX_DOMAIN_NAME} ${NGINX_SUB_DOMAIN_NAME} ${NGINX_REPOSITORY}' < templates/certbot_sub_domain_static.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
 ln -s /etc/nginx/sites-available/${NGINX_DOMAIN_NAME} /etc/nginx/sites-enabled/${NGINX_DOMAIN_NAME}
 # Copy a new config file
 cp conf/nginx.conf /etc/nginx/nginx.conf
