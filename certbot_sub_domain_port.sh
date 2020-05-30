46b5fe7ef7827682ce19521e07f914057bff61c1
Evann BACALA <evann.bacala@epsi.fr>
2020-05-30 16:36:02 +0200

    fixed variables issues

--- certbot_sub_domain_port.sh
+++ certbot_sub_domain_port.sh
@@ -15,9 +15,9 @@ apt install -y nginx certbot python3-certbot-nginx
 fuser -k 80/tcp
 service nginx restart
 
-export NGINX_DOMAIN_NAME=$nginx_domain_name
-export NGINX_EMAIL_ADDRESS=$nginx_email_address
-export NGINX_APP_PORT=$nginx_app_port
+export NGINX_DOMAIN_NAME=${nginx_domain_name}
+export NGINX_EMAIL_ADDRESS=${nginx_email_address}
+export NGINX_APP_PORT=${nginx_app_port}
 
 # Launch Certbot with the domain name and email address defined in the environment variables
 certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agree-tos --no-eff-email
@@ -26,7 +26,7 @@ certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agr
 openssl dhparam -out /etc/letsencrypt/live/${NGINX_DOMAIN_NAME}/dhparam.pem 2048
 
 # Replace the default template file with the environment variables
-envsubst '${NGINX_DOMAIN_NAME} ${NGINX_SUB_DOMAIN_NAME} ${NGINX_APP_PORT}' < templates/certbot_sub_domain_port.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
+envsubst '${NGINX_DOMAIN_NAME} ${NGINX_APP_PORT}' < templates/certbot_sub_domain_port.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
 ln -s /etc/nginx/sites-available/${NGINX_DOMAIN_NAME} /etc/nginx/sites-enabled/${NGINX_DOMAIN_NAME}
 # Copy a new config file
 cp conf/nginx.conf /etc/nginx/nginx.conf
