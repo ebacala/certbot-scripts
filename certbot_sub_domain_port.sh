1b0d84ba5e95670f545ccf35d5c05e0559cfd28b
Evann BACALA <dev@evannbacala.fr>
2020-11-11 16:10:55 +0100

    fix: allow script to be launched from outside its main directory

--- certbot_sub_domain_port.sh
+++ certbot_sub_domain_port.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 echo "Please enter your domain name:"
 read nginx_domain_name
 echo "Please enter your email adress (only used for certbot script):"
@@ -26,14 +28,14 @@ certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agr
 openssl dhparam -out /etc/letsencrypt/live/${NGINX_DOMAIN_NAME}/dhparam.pem 2048
 
 # Replace the default template file with the environment variables
-envsubst '${NGINX_DOMAIN_NAME} ${NGINX_APP_PORT}' < templates/certbot_sub_domain_port.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
+envsubst '${NGINX_DOMAIN_NAME} ${NGINX_APP_PORT}' < $DIR/templates/certbot_sub_domain_port.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
 ln -s /etc/nginx/sites-available/${NGINX_DOMAIN_NAME} /etc/nginx/sites-enabled/${NGINX_DOMAIN_NAME}
 
 # Copy a new config file
 echo "Do you want to update your nginx global config file (you can see this config file in certbot-scripts/conf/nginx.conf) ? (y/n):"
 read nginx_update_config
 if [[ $nginx_update_config == "y" ]]; then
-    cp conf/nginx.conf /etc/nginx/nginx.conf
+    cp $DIR/conf/nginx.conf /etc/nginx/nginx.conf
     echo "Your nginx global config file has been updated"
 fi
 
--- certbot_sub_domain_static.sh
+++ certbot_sub_domain_static.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 echo "Please enter your domain name:"
 read nginx_domain_name
 echo "Please enter your email adress (only used for certbot script):"
@@ -26,14 +28,14 @@ certbot --nginx certonly -d ${NGINX_DOMAIN_NAME} -m ${NGINX_EMAIL_ADDRESS} --agr
 openssl dhparam -out /etc/letsencrypt/live/${NGINX_DOMAIN_NAME}/dhparam.pem 2048
 
 # Replace the default template file with the environment variables
-envsubst '${NGINX_DOMAIN_NAME} ${NGINX_REPOSITORY}' < templates/certbot_sub_domain_static.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
+envsubst '${NGINX_DOMAIN_NAME} ${NGINX_REPOSITORY}' < $DIR/templates/certbot_sub_domain_static.template > /etc/nginx/sites-available/${NGINX_DOMAIN_NAME}
 ln -s /etc/nginx/sites-available/${NGINX_DOMAIN_NAME} /etc/nginx/sites-enabled/${NGINX_DOMAIN_NAME}
 
 # Copy a new config file
 echo "Do you want to update your nginx global config file? (y/n):"
 read nginx_update_config
 if [[ $nginx_update_config == "y" ]]; then
-    cp conf/nginx.conf /etc/nginx/nginx.conf
+    cp $DIR/conf/nginx.conf /etc/nginx/nginx.conf
 fi
 
 # Reload the nginx configuration
