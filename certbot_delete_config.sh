e09aefc8b567cc58b5392ea4025483555d1b0fd0
Evann BACALA <dev@evannbacala.fr>
2020-11-11 16:38:37 +0100

    feat: added delete script

---
+++ certbot_delete_config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Please enter the name of the config file to delete:"
+read nginx_config_file_name
+
+# Delete Let's Encrypt certificate
+certbot delete --cert-name ${nginx_config_file_name}
+
+# Delete Nginx configuration file
+rm /etc/nginx/sites-available/${nginx_config_file_name} /etc/nginx/sites-enabled/${nginx_config_file_name}
+
+# Delete Let's Encrypt directory for the configuration file name (because dhparams can still be there)
+rm -rf /etc/letsencrypt/live/${nginx_config_file_name}
+
+# Restart Nginx
+service nginx restart
